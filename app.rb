fee892c28e7438098ca62f4f378b833528e453b3
axenoff <axenoff@gmail.com>
2017-03-05 13:16:13 +0300

    Create product migration

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzeria.db"
+
+class Product < ActiveRecord::Base
+
+end
 
 get '/' do
 	erb :index
