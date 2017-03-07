bbba9143397575257d2bf4007922773ab8ed2c58
axenoff <axenoff@gmail.com>
2017-03-07 11:13:43 +0300

    Add to cart with JS

--- public/app.js
+++ public/app.js
@@ -5,3 +5,14 @@ function something()
 	window.localStorage.setItem('bbb', x);
 	alert(x);
 } 
+
+function add_to_cart(id)
+{
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x*1+1;
+	window.localStorage.setItem(key, x);
+}
+
+ 
